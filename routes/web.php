3fb41b1e4f83c9b91a5d6ba5a139e6afa5e2be35
Jeferson Guilherme <jefersonguilherme.pe@gmail.com>
2024-12-03 15:05:04 -0300

    Adicionando autenticação as rotas e limitando acesso

--- routes/web.php
+++ routes/web.php
@@ -6,27 +6,20 @@ use App\Http\Controllers\Admin\StoreController;
 
 Route::get('/admin/stores', [StoreController::class, 'index']);
 
-
 Route::get('/', function () { return view('welcome'); });
 
-Route::prefix('admin')->as('admin.')->group(function () {
+Route::group(['middleware' => ['auth']], function () {
+    Route::prefix('admin')->as('admin.')->group(function () {
 
-    // Subgrupo para 'stores'
-    /*Route::prefix('stores')->as('stores.')->group(function () {
-        Route::get('/', [StoreController::class, 'index'])->name('index');
-        Route::get('/create', [StoreController::class, 'create'])->name('create');
-        Route::post('/store', [StoreController::class, 'store'])->name('store');
-        Route::get('/{store}/edit', [StoreController::class, 'edit'])->name('edit');
-        Route::post('/update/{store}', [StoreController::class, 'update'])->name('update');
-        Route::get('/destroy/{store}', [StoreController::class, 'destroy'])->name('destroy');
-    });*/
-    Route::resource('stores', StoreController::class);
+        // Subgrupo para 'stores'
+        Route::resource('stores', StoreController::class);
 
-    //SubGrupo para 'Products'
-    Route::resource('products', ProductController::class);
+        //SubGrupo para 'Products'
+        Route::resource('products', ProductController::class);
 
-    //Route::prefix('products')->as('product.')->group(function () {
-    //    Route::get('/', [ProductController::class, 'index'])->name('index');
-    //});
-
+    });
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
